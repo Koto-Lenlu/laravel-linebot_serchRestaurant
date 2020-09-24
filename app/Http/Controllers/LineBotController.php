eb3b7752aa4a7959fb44fd3b5c829b526fe0f6db
Koto-Lenlu <takeda.koto.555@gmail.com>
2020-09-24 20:49:59 +0900

    Update LineBotController.php
    ・LineBotクラスを生成

--- app/Http/Controllers/LineBotController.php
+++ app/Http/Controllers/LineBotController.php
@@ -3,8 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
 use Illuminate\Support\Facades\Log;
+use LINE\LINEBot;
+use LINE\LINEBot\HTTPClient\CurlHTTPClient;
 
 class LineBotController extends Controller
 {
@@ -12,10 +13,13 @@ class LineBotController extends Controller
     {
         return view('linebot.index');
     }
-    
+
     public function parrot(Request $request)
     {
         Log::debug($request->header());
         Log::debug($request->input());
+
+        $httpClient = new CurlHTTPClient(env('LINE_ACCESS_TOKEN'));
+        $lineBot = new LINEBot($httpClient, ['channelSecret' => env('LINE_CHANNEL_SECRET')]);
     }
 }
